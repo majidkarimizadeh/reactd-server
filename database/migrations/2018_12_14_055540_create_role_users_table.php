a7fec3a9118f5fbb3897c7193d41fdff29d5d523
majid karimi zadeh <mj.karimizadeh@gmail.com>
2018-12-29 14:06:08 +0330

    add relation for role users

--- database/migrations/2018_12_14_055540_create_role_users_table.php
+++ database/migrations/2018_12_14_055540_create_role_users_table.php
@@ -19,6 +19,14 @@ class CreateRoleUsersTable extends Migration
             $table->nullableTimestamps();
 
             $table->primary(['user_id', 'role_id']);
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('role_id')
+                ->references('id')->on('roles')
+                ->onDelete('cascade');
         });
     }
 
